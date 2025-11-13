5e2f0c6a5c05666632c881ada93b193171dd56a2
Ansh Gangwar <anshgangwar998@gmail.com>
2025-11-13 23:46:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toCase(text) {
-  // write your code here
+  let lc = text.toLowerCase();
+  let uc = text.toUpperCase();
+  let str = lc + "-" + uc;
+  return str;
 }
 
 // DO not change the code below
-
 const text = prompt("Enter text:");
 alert(toCase(text));
